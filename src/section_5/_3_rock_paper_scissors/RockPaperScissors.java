72d0d53c930ec52ff9eb94f7e28a5b664cd7e33c
mjfre <mjfre@aol.com>
2021-06-20 15:20:01 -0700

    Updating Rock Paper Scissors

--- src/section_5/_3_rock_paper_scissors/RockPaperScissors.java
+++ src/section_5/_3_rock_paper_scissors/RockPaperScissors.java
@@ -3,7 +3,7 @@
 
 package section_5._3_rock_paper_scissors;
 
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,36 +15,66 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class RockPaperScissors extends JPanel implements ActionListener{
+public class RockPaperScissors extends JPanel implements ActionListener {
 
-	private JFrame window = new JFrame("Rock Paper Scissors");
-	private JButton rockButton = new JButton();
-	private JButton paperButton = new JButton();
-	private JButton scissorsButton = new JButton();
+    //If you don't like these images, you can add "2" to the end of each (e.g. "./img/rock2.png")
+    private final String rockImageUrl = "./img/rock.png";
+    private final String paperImageUrl = "./img/paper.png";
+    private final String scissorsImageUrl = "./img/scissors.png";
 
-	private JLabel instructionLabel = new JLabel();
+    public static void main(String[] args) {
+        new RockPaperScissors().createWindow();
+    }
 
-	private Icon rockImage;
-	private Icon paperImage;
-	private Icon scissorsImage;
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-	private Dimension buttonDim = new Dimension(300, 200);
+        //1. Run the program and click and image to make a choice. Notice that the computer always makes the same choice.
 
-    public void run(){
+        //2. Change the value of opponentSelection to be a random number between 0 and 2;
+        //   Don't forget to create a Random object.
 
-		try {
+        int opponentSelection = 0;
 
-			rockImage = new ImageIcon(getClass().getResource("rock.png"));
-			paperImage = new ImageIcon(getClass().getResource("paper.jpeg"));
-			scissorsImage = new ImageIcon(getClass().getResource("scissors.jpeg"));
+        //3. Run the program again. Is the result different?
 
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null, "Cannot find one or more of your images");
-		}
+        int selection = 0;
 
-		rockButton.setIcon(rockImage);
-		paperButton.setIcon(paperImage);
-		scissorsButton.setIcon(scissorsImage);
+        if (e.getSource() == paperButton) {
+            selection = 1;
+        } else if (e.getSource() == scissorsButton) {
+            selection = 2;
+        }
+
+        JOptionPane.showMessageDialog(null, "You chose: " + convertSelection(selection) + ".\n"
+                + "The computer chose: " + convertSelection(opponentSelection) + ".\n");
+
+        if (selection == opponentSelection) {
+            JOptionPane.showMessageDialog(null, "No Winner.  Play again.");
+        } else if ((selection == 0 && opponentSelection == 2) ||
+                (selection == 1 && opponentSelection == 0) ||
+                (selection == 2 && opponentSelection == 1)) {
+            JOptionPane.showMessageDialog(null, "You Win!");
+        } else {
+            JOptionPane.showMessageDialog(null, "You Lose!");
+        }
+    }
+
+    private String convertSelection(int s) {
+        if (s == 0)
+            return "ROCK";
+        else if (s == 1)
+            return "PAPER";
+        else if (s == 2)
+            return "SCISSORS";
+        else
+            return "";
+    }
+
+    public void createWindow() {
+        rockButton.setIcon(new ImageIcon(rockImage));
+        paperButton.setIcon(new ImageIcon(paperImage));
+        scissorsButton.setIcon(new ImageIcon(scissorsImage));
 
         rockButton.addActionListener(this);
         paperButton.addActionListener(this);
@@ -53,7 +83,7 @@ public class RockPaperScissors extends JPanel implements ActionListener{
         rockButton.setPreferredSize(buttonDim);
         paperButton.setPreferredSize(buttonDim);
         scissorsButton.setPreferredSize(buttonDim);
-
+        setBackground(new Color(80, 134, 138));
         instructionLabel.setText("Choose Your Weapon!");
 
         add(instructionLabel);
@@ -67,54 +97,17 @@ public class RockPaperScissors extends JPanel implements ActionListener{
         window.pack();
     }
 
-    public static void main(String[] args) {
-        new RockPaperScissors().run();
-    }
+    private JFrame window = new JFrame("Rock, Paper, Scissors");
+    private JButton rockButton = new JButton();
+    private JButton paperButton = new JButton();
+    private JButton scissorsButton = new JButton();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private JLabel instructionLabel = new JLabel();
 
-        //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing?
+    private Image rockImage = new ImageIcon(getClass().getResource(rockImageUrl)).getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
+    private Image paperImage = new ImageIcon(getClass().getResource(paperImageUrl)).getImage().getScaledInstance(250, 200, Image.SCALE_DEFAULT);
+    private Image scissorsImage = new ImageIcon(getClass().getResource(scissorsImageUrl)).getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
 
-        //2. Change the value of opponentSelection to be a random number between 0 and 2;
-        //   Don't forget to create a Random object.
+    private Dimension buttonDim = new Dimension(300, 200);
 
-        //2. Change the value of opponentSelection to be a random number between 0 and 2;
-        int opponentSelection = 0;
-
-        //3. Run the program again. Is the result different?
-
-        int selection = 0;
-
-        if(e.getSource() == paperButton){
-            selection = 1;
-        }else if(e.getSource() == scissorsButton){
-            selection = 2;
-        }
-
-        JOptionPane.showMessageDialog(null, "You chose: " + convertSelection(selection) + ".\n"
-                    + "The computer chose: " + convertSelection(opponentSelection) + ".\n");
-
-        if(selection == opponentSelection){
-            JOptionPane.showMessageDialog(null, "No Winner.  Play again.");
-        }else if((selection == 0 && opponentSelection == 2) ||
-                 (selection == 1 && opponentSelection == 0) ||
-                 (selection == 2 && opponentSelection == 1)){
-            JOptionPane.showMessageDialog(null, "You Win!");
-        }else{
-            JOptionPane.showMessageDialog(null, "You Lose!");
-        }
-    }
-
-    private String convertSelection(int s){
-       if (s==0)
-            return "ROCK";
-       else if (s==1)
-            return "PAPER";
-       else if (s==2)
-            return "SCISSORS";
-       else
-            return "";
-        }
-    }
-
+}
